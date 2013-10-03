718f12e7631f450c8826f4f1c57fa0e4d5d37587
malnick <malnick@gmail.com>
2013-10-03 09:38:14 -0700

    wget line

---
+++ autogen.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Auto generate resume based on perl dict and template resume file
+# Jeff Malnick
+# October 3 2013
+
+for n in $#
+	do
+		wget -O job{$n}.html $1 && echo "Download of " $1 " successful" || echo "Download failed for " $i 
+	done
+#http://inflection.com/careers/information-technology--senior-linux-system-administrator-jobs.html
+
+
