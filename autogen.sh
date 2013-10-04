599c244ab9e11655561a4616a8f9dfaf08a2a600
malnick <malnick@gmail.com>
2013-10-04 11:02:29 -0700

    array access

--- autogen.sh
+++ autogen.sh
@@ -2,11 +2,22 @@
 # Auto generate resume based on perl dict and template resume file
 # Jeff Malnick
 # October 3 2013
+#
+# Dependancies:
+#
+# 	Template Toolkit: http://template-toolkit.org/download/
+#
+#####
 
 for n in $#
 	do
 		wget -O job{$n}.html $1 && echo "Download of " $1 " successful" || echo "Download failed for " $i 
 	done
+	
+	
+	
+	
+	
 #http://inflection.com/careers/information-technology--senior-linux-system-administrator-jobs.html
 
 
