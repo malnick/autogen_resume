bd8f5443ca7eaa09d2f20b0f1c910543c204e4c3
malnick <malnick@gmail.com>
2013-10-04 11:04:17 -0700

    array access

---
+++ resume_template.pl
@@ -0,0 +1,45 @@
+use Template;
+
+# some useful options (see below for full list)
+my $config = {
+	INTERPOLATE  => 1,               # expand "$var" in plain text
+	POST_CHOMP   => 1,               # cleanup whitespace 
+	PRE_PROCESS  => 'header',        # prefix each template
+	EVAL_PERL    => 1,               # evaluate Perl code blocks
+};
+
+# create Template object
+my $template = Template->new($config);
+
+# define template variables for replacement
+my %adverbs = (
+		communication => ['Advocated', 'Clarified', 'Corresponded', 'Encouraged', 'Interpreted', 'Negotiated', 'Persuaded', 'Presented', 'Publicized', 'Solicited', 'Spoke', 'Translated'],
+		creative => ['Acted', 'Applied', 'Composed', 'Created', 'Established', 'Founded', 'Improvised', 'Introduced', 'Navigated', 'Originated', 'Presented'],
+		data => ['Adjusted', 'Allocated', 'Budgeted', 'Compared', 'Computed', 'Counted', 'Documented', 'Estimated', 'Forecasted', 'Inventoried', 'Invested', 'Predicted', 'Projected', 'Quantified', 'Recorded', 'Retrieved', 'Verified'],
+		help => ['Aided', 'Assisted', 'Built', 'Demonstrated', 'Facilitated', 'Familiarized', 'Helped', 'Performed', 'Represented', 'Solved', 'Supported', 'Trained', 'Upheld', 'Volunteered', 'Worked'],
+		leadership => ['Achieved', 'Administered', 'Assigned', 'Attained', 'Challenged', 'Coordinated', 'Decided', 'Delegated', 'Established', 'Executed', 'Handled', 'Headed', 'Implemented', 'Incorporated', 'Intervened', 'Launched', 'Led', 'Managed', 'Mediated', 'Motivated', 'Organized', 'Oversaw', 'Planned', 'Prioritized', 'Recommended', 'Scheduled', 'Supervised', 'United'],
+		efficient => ['Accelerated', 'Allocated', 'Boosted', 'Centralized', 'Downsized', 'Edited', 'Eliminated', 'Enhanced', 'Expanded', 'Expedited', 'Heightened', 'Lessened', 'Leveraged', 'Maximized', 'Merged', 'Optimized', 'Outlined', 'Outsourced', 'Prevented', 'Prioritized', 'Reorganized', 'Reduced', 'Revised', 'Simplified', 'Standardized', 'Stream-lined', 'Synthesized', 'Systematized', 'Upgraded'],
+		technical => ['Analyzed', 'Assembled', 'Built', 'Calculated', 'Computed', 'Conducted', 'Designed', 'Devised', 'Engineered', 'Maintained', 'Operated', 'Programmed', 'Reengineered', 'Remodeled', 'Transmitted'],
+		skills => ['Ruby', 'Perl', 'Python', 'Java', 'Puppet', 'Linux', 'Red Hat', 'RHEL', 'Debian', 'Ubuntu', 'Shell', 'Bash'],
+		);
+
+# Hash of hashes for template 
+my %data = 	( adverbs => \@adverbs,
+	
+			};
+
+# Access loop for individual words in hash of adverbs
+for $key ( keys %adverbs ) {
+    print "$key:\n";
+    for $word (0 .. $#{$adverbs{$key}}){
+    	print "$word = $adverbs{$key}[$word]\n";
+    	}
+    print "\n";
+
+
+# specify input filename, or file handle, text reference, etc.
+my $input = 'myfile.html';
+
+# process input template, substituting variables
+$template->process($input, $vars)
+  || die $template->error();
